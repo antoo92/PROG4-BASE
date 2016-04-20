a9f77c79b63c64d941a1194f0de16f05329c83dd
antoine.bourlegat <antoine.bourlegat@LCET1111.cwbdis.pucpr.br>
2016-04-20 18:35:59 -0300

    vbase

---
+++ base/src/main/java/cwb/pucpr/br/prog4/BaseApplication.java
@@ -0,0 +1,12 @@
+package cwb.pucpr.br.prog4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApplication.class, args);
+	}
+}
